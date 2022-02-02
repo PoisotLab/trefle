cfc500fbee9a12040bfd87305a4fca37ae12adfe
Tanya Strydom <tanya.strydom@icloud.com>
2022-02-02 15:18:02 -0500

    okay so that imports virionette and both tuning and imputation scripts are doing things

--- hpc/inputs/read_clover.jl
+++ hpc/inputs/read_clover.jl
@@ -1,8 +1,8 @@
-clover_df = "clover.csv" |> CSV.File |> DataFrame
-hosts = sort(unique(clover_df.Host))
-viruses = sort(unique(clover_df.Virus))
+clover_df = "virionette.csv" |> CSV.File |> DataFrame
+hosts = sort(unique(clover_df.host_species))
+viruses = sort(unique(clover_df.virus_genus))
 A = zeros(Bool, (length(viruses), length(hosts)))
 clover = BipartiteNetwork(A, viruses, hosts)
 for clover_row in eachrow(clover_df)
-    clover[clover_row.Virus, clover_row.Host] = true
+    clover[clover_row.virus_genus, clover_row.host_species] = true
 end
